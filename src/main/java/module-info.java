1b70121798d7a9a3ba3c7fde2390a192f8e7d6e9
y785 <j7852398521@gmail.com>
2020-09-22 04:25:54 -0500

    module-info.java
    bugfix.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,10 @@
 module moe.maple.scheduler {
     exports moe.maple.scheduler;
+    exports moe.maple.scheduler.target;
+    exports moe.maple.scheduler.target.delay;
     exports moe.maple.scheduler.tasks;
     exports moe.maple.scheduler.tasks.delay;
+    exports moe.maple.scheduler.tasks.repeat;
+    exports moe.maple.scheduler.tasks.retry;
     exports moe.maple.scheduler.tasks.tick;
 }
